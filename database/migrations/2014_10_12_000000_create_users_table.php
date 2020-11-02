d4cf431eb4a75e1c6a07d1a224ac25b109b8da23
vagrant <vagrant@homestead>
2020-11-02 06:55:07 +0000

    Created login-auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,19 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->increments('id');
+            $table->string('name',50);
+            $table->enum('gender', ['man', 'woman', 'others', 'no answer'])->nullable();
+            $table->dateTime('date_of_birth')->nullable(); // modelでフォーマットはキャスト済
+            $table->string('tel', 20)->nullable();
+            $table->string('address',50)->nullable();
+            $table->string('email',50)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->boolean('delete_flg')->default(0);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
         });
     }
 
